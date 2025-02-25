482ce19e6778f9351c9c35778b52c0f3a5988661
Shermaine Suah <shermaine.suah@gmail.com>
2025-02-25 18:00:11 +0800

    Updated tailwind's theme to color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#0A2540", // Teal Blue (Primary)
+        secondary: "#0077B6", // Dark Navy (Secondary)
+        gray: {
+          DEFAULT: "#f4f6fc", // Cool Gray (Background)
+          text: "#5C677D", // Secondary Text
+          border: "#D1D5DB", // Borders
+        },
+        success: "#2BA84A", // Success Green
+        warning: "#F4A261", // Warning Orange
+        error: "#D62828", // Error Red
+        text: {
+          primary: "#1C1C1C", // Primary Text
+          secondary: "#5C677D", // Secondary Text
+        },
+      },
+    },
   },
   plugins: [],
 };
