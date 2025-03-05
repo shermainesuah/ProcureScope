a0bd10576992f2d61fcab6a4bada60a416a5513e
Shermaine Suah <shermaine.suah@gmail.com>
2025-03-05 21:26:10 +0800

    Updated navbar to use Link

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,33 +1,37 @@
 import { CircleUser, FileSpreadsheet, LogOut } from "lucide-react";
-import logo from "../assets/logo.svg"; // Import your logo from src/assets
-import { useNavigate } from "react-router-dom";
+import logo from "../assets/logo.svg";
+import { useNavigate, useLocation, Link } from "react-router-dom";
 
-const Navbar: React.FC = () => {
+const Navbar = () => {
   const navigate = useNavigate();
+  const currentLocation = useLocation();
+
   return (
     <nav className=" text-primary px-8 py-4 flex items-center justify-between">
-      <div
-        className="flex items-center gap-2 hover:cursor-pointer"
-        onClick={() => navigate("/dashboard")}
-      >
-        <img src={logo} alt="ProcureScope Logo" className="h-8 w-8" />
-        <span className="text-xl font-bold font-mono">ProcureScope</span>
-      </div>
-      <div className="flex gap-7">
-        <FileSpreadsheet
-          onClick={() => {
-            navigate("/data-import");
-          }}
-          className="h-5 w-5 text-textColor-primary hover:text-secondary hover:cursor-pointer"
-        />
-        <CircleUser className="h-5 w-5 text-textColor-primary hover:text-secondary hover:cursor-pointer" />
-        <LogOut
-          onClick={() => {
-            navigate("/login");
-          }}
-          className="h-5 text-textColor-primary hover:text-secondary w-5 hover:cursor-pointer"
-        />
-      </div>
+      <Link to="/dashboard">
+        <div className="flex items-center gap-2 hover:cursor-pointer">
+          <img src={logo} alt="ProcureScope Logo" className="h-8 w-8" />
+          <span className="text-xl font-bold font-mono">ProcureScope</span>
+        </div>
+      </Link>
+      {currentLocation.pathname === "/dashboard" && (
+        <div className="flex gap-7">
+          <Link to="/data-import">
+            <FileSpreadsheet className="h-5 w-5 text-textColor-primary hover:text-secondary hover:cursor-pointer" />
+          </Link>
+          <Link to="">
+            <CircleUser className="h-5 w-5 text-textColor-primary hover:text-secondary hover:cursor-pointer" />
+          </Link>
+          <Link to="/login">
+            <LogOut
+              onClick={() => {
+                navigate("/login");
+              }}
+              className="h-5 text-textColor-primary hover:text-secondary w-5 hover:cursor-pointer"
+            />
+          </Link>
+        </div>
+      )}
     </nav>
   );
 };
