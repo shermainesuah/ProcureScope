24dc9bec65dd0be7ed0311037d382df005f0d85c
Shermaine Suah <shermaine.suah@gmail.com>
2025-03-06 23:36:03 +0800

    Update max width for dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -6,13 +6,13 @@ const Dashboard: React.FC = () => {
   return (
     <div className="main-container flex space-y-8 flex-col">
       <h1>Dashboard</h1>
-      <div className="p-6 border rounded-lg max-w-6xl">
+      <div className="p-6 border rounded-lg max-w-5xl">
         <AnomaliesAlert />
       </div>
-      <div className="p-6 border rounded-lg max-w-6xl">
+      <div className="p-6 border rounded-lg max-w-5xl">
         <GlobalProcurement />
       </div>
-      <div className="p-6 border rounded-lg max-w-6xl">
+      <div className="p-6 border rounded-lg max-w-5xl">
         <ProcurementTrendsForecasting />
       </div>
     </div>
